6a8a2af9e9f9ffb8cbebd281a95eb09fe69ca62b
sundev-nalees <72244488+sundev-nalees@users.noreply.github.com>
2022-01-10 00:12:23 +0530

    controls

---
+++ Assets/script/food.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour
+{
+    public BoxCollider2D areaGrid;
+    void Start()
+    {
+        randomFoodPosition();
+    }
+    private void randomFoodPosition()
+    {
+        Bounds bounds = this.areaGrid.bounds;
+
+        float x = Random.Range(bounds.min.x,bounds.max.x);
+        float y = Random.Range(bounds.min.y,bounds.max.y);
+
+        this.transform.position = new Vector3(x, y, 0f);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            randomFoodPosition();
+        }
+        else if (other.tag == "Player2")
+        {
+            randomFoodPosition();
+        }
+
+
+
+
+    }
+}
---
+++ Assets/script/gameOver.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameOver : MonoBehaviour
+{
+    public Button restart;
+    
+    public Button mainMenu;
+    private void Awake()
+    {
+        restart.onClick.AddListener(restartGame);
+        mainMenu.onClick.AddListener(mainMenuScene);
+
+    }
+    public void playerDead()
+    {
+        gameObject.SetActive(true);
+
+
+    }
+    private void restartGame()
+    {
+        int currentSceneNumber = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneNumber);
+        
+    }
+    private void mainMenuScene()
+    {
+        //SceneManager.LoadScene(levelN);
+        
+
+    }
+}
